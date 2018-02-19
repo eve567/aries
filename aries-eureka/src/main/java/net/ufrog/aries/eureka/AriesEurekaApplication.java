7cac5447684600380357ac204a92e1ee5e9706a5
ultrafrog <ufrog.net@gmail.com>
2018-02-19 21:44:06 +0800

    更新项目名称

--- aries-eureka/src/main/java/net/ufrog/aries/eureka/AriesEurekaApplication.java
+++ aries-eureka/src/main/java/net/ufrog/aries/eureka/AriesEurekaApplication.java
@@ -1,4 +1,4 @@
-package net.ufrog.aries;
+package net.ufrog.aries.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,17 +6,17 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @author ultrafrog, ufrog.net@gmail.com
- * @version 0.1, 2018-02-18
+ * @version 0.1, 2018-02-19
  * @since 0.1
  */
 @SpringBootApplication
 @EnableEurekaServer
-public class AriesServerApplication {
+public class AriesEurekaApplication {
 
     /**
      * @param args argument array
      */
     public static void main(String[] args) {
-        SpringApplication.run(AriesServerApplication.class, args);
+        SpringApplication.run(AriesEurekaApplication.class, args);
     }
 }
